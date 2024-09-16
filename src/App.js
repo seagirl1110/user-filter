1c5d1ed98cd66838feb7b8c8dc29c030b495f9b2
Marina Elizarova <seagirl1110@gmail.com>
2024-09-16 12:44:48 +0300

    feat: add component Filter

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import './App.css';
 import React from 'react';
+import Filter from './components/Filter';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Filter />
+    </div>
+  );
 }
 
 export default App;
--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,8 +1,34 @@
 import styles from './../styles/Filter.module.css';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { connect } from 'react-redux';
+import { setFilter } from './../redux/actions';
 
-function Filter() {
-  return <div></div>;
+function Filter({ filterValue, setFilterValue }) {
+  const [value, setValue] = useState('');
+
+  useEffect(() => {
+    filterValue && setValue(filterValue);
+  }, []);
+
+  const handleChange = (evt) => {
+    const newValue = evt.target.value;
+    setValue(newValue);
+    setFilterValue(newValue);
+  };
+
+  return (
+    <div>
+      <input value={value} onChange={handleChange} type="text" />
+    </div>
+  );
 }
 
-export default Filter;
+const mapStateToProps = (state) => ({
+  filterValue: state.filterValue,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  setFilterValue: (value) => dispatch(setFilter(value)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Filter);
